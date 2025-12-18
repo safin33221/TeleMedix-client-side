a70055a71e91959864c450d5e9d3116214705d67
safin33221 <safin33221@gmail.com>
2025-12-18 19:44:23 +0600

    added reusebale server fetch

---
+++ src/lib/server-fetch.ts
@@ -0,0 +1,35 @@
+import { getCookies } from "@/services/auth/tokenHandler";
+
+const BackendBaseUrl = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000/api/v1";
+
+
+
+const serverFetchHelper = async (endpoint: string, options: RequestInit): Promise<Response> => {
+
+    const { headers, ...restOptions } = options
+    const accessToken = await getCookies("accessToken")
+    const response = await fetch(`${BackendBaseUrl}${endpoint}`, {
+        headers: {
+            ...headers,
+            Cookie: accessToken ? `accessToken=${accessToken}` : ''
+        },
+        ...restOptions
+
+    })
+    return response
+}
+
+
+export const serverFetch = {
+
+    get: async (endpoint: string, options: RequestInit = {}): Promise<Response> => serverFetchHelper(endpoint, { ...options, method: "GET" }),
+
+    put: async (endpoint: string, options: RequestInit = {}): Promise<Response> => serverFetchHelper(endpoint, { ...options, method: "PUT" }),
+
+    post: async (endpoint: string, options: RequestInit = {}): Promise<Response> => serverFetchHelper(endpoint, { ...options, method: "POST" }),
+
+    delete: async (endpoint: string, options: RequestInit = {}): Promise<Response> => serverFetchHelper(endpoint, { ...options, method: "DELETE" }),
+
+
+
+}
